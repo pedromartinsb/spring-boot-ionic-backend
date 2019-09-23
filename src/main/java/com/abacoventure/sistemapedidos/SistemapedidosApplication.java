671e1fe728f6fd17fe104ca0ff60d8a3b2b231c3
Pedro Martins <engpedrocampos@gmail.com>
2019-09-23 15:44:46 -0300

    Projeto criado

---
+++ src/main/java/com/abacoventure/sistemapedidos/SistemapedidosApplication.java
@@ -0,0 +1,13 @@
+package com.abacoventure.sistemapedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemapedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemapedidosApplication.class, args);
+	}
+
+}
