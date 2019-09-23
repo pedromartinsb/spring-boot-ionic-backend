fa5dbb56ccbb7963934ded3676e5fbf208250f6a
Pedro Martins <engpedrocampos@gmail.com>
2019-09-23 18:33:00 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/abacoventure/sistemapedidos/services/CategoriaService.java
+++ src/main/java/com/abacoventure/sistemapedidos/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.abacoventure.sistemapedidos.domain.Categoria;
 import com.abacoventure.sistemapedidos.repositories.CategoriaRepository;
+import com.abacoventure.sistemapedidos.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(()->new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }
