d90de267f7e2635e9c503abc3dc7a1d62fdce515
Pedro Martins <engpedrocampos@gmail.com>
2019-09-23 23:36:16 -0300

    Estado e cidade

--- src/main/java/com/abacoventure/sistemapedidos/SistemapedidosApplication.java
+++ src/main/java/com/abacoventure/sistemapedidos/SistemapedidosApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.abacoventure.sistemapedidos.domain.Categoria;
+import com.abacoventure.sistemapedidos.domain.Cidade;
+import com.abacoventure.sistemapedidos.domain.Estado;
 import com.abacoventure.sistemapedidos.domain.Produto;
 import com.abacoventure.sistemapedidos.repositories.CategoriaRepository;
+import com.abacoventure.sistemapedidos.repositories.CidadeRepository;
+import com.abacoventure.sistemapedidos.repositories.EstadoRepository;
 import com.abacoventure.sistemapedidos.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class SistemapedidosApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SistemapedidosApplication.class, args);
@@ -39,10 +47,24 @@ public class SistemapedidosApplication implements CommandLineRunner {
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat1));		
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 	
 	
---
+++ src/main/java/com/abacoventure/sistemapedidos/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.abacoventure.sistemapedidos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.abacoventure.sistemapedidos.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
