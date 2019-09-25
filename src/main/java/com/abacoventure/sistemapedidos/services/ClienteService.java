683ad35bed89360830cfd4f8d698caebe581b744
Pedro Martins <engpedrocampos@gmail.com>
2019-09-25 14:50:40 -0300

    Ajustando e testando a delecao de clientes

--- src/main/java/com/abacoventure/sistemapedidos/services/ClienteService.java
+++ src/main/java/com/abacoventure/sistemapedidos/services/ClienteService.java
@@ -60,7 +60,7 @@ public class ClienteService {
 		} 
 		catch (DataIntegrityViolationException e) 
 		{
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 		
 	}
