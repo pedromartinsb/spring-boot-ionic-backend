3706c0159457e16c541bcceee32b27db9db7e732
Pedro Martins <engpedrocampos@gmail.com>
2019-09-26 11:13:22 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/abacoventure/sistemapedidos/repositories/ClienteRepository.java
+++ src/main/java/com/abacoventure/sistemapedidos/repositories/ClienteRepository.java
@@ -1,15 +1,14 @@
 package com.abacoventure.sistemapedidos.repositories;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.abacoventure.sistemapedidos.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
-	@Transactional()
+	@Transactional(readOnly=true)
 	Cliente findByEmail(String email);
 }
---
+++ src/main/java/com/abacoventure/sistemapedidos/services/ProdutoService.java
@@ -0,0 +1,39 @@
+package com.abacoventure.sistemapedidos.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.abacoventure.sistemapedidos.domain.Categoria;
+import com.abacoventure.sistemapedidos.domain.Produto;
+import com.abacoventure.sistemapedidos.repositories.CategoriaRepository;
+import com.abacoventure.sistemapedidos.repositories.ProdutoRepository;
+import com.abacoventure.sistemapedidos.services.exception.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(()->new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+	
+}
