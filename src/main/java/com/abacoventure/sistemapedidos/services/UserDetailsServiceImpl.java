9a2c961608933b82c5acad3e6994c19cbf22a73b
Pedro Martins <engpedrocampos@gmail.com>
2019-09-27 17:14:01 -0300

    Implementacao autenticacao e geracaodo token JWT

---
+++ src/main/java/com/abacoventure/sistemapedidos/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.abacoventure.sistemapedidos.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.abacoventure.sistemapedidos.domain.Cliente;
+import com.abacoventure.sistemapedidos.repositories.ClienteRepository;
+import com.abacoventure.sistemapedidos.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
