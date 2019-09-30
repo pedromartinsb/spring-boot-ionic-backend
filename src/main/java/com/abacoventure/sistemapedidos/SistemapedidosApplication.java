2c399a032e6e1ca27f428454905c954620b5dce7
Pedro Martins <engpedrocampos@gmail.com>
2019-09-30 12:39:16 -0300

    Salvando primeiro arquivo no s3

--- src/main/java/com/abacoventure/sistemapedidos/SistemapedidosApplication.java
+++ src/main/java/com/abacoventure/sistemapedidos/SistemapedidosApplication.java
@@ -1,18 +1,25 @@
 package com.abacoventure.sistemapedidos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.abacoventure.sistemapedidos.services.S3Service;
+
 @SpringBootApplication
-public class SistemapedidosApplication implements CommandLineRunner {	
+public class SistemapedidosApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SistemapedidosApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {		
+	public void run(String... args) throws Exception {	
+		s3Service.uploadFile("C:\\Users\\Pedro\\Desktop\\PastaPedro\\Outros\\icones\\clientes.png");
 	}	
 
 }
