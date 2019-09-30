e67ed6d3e90f922106e30612acf7083aad04dadc
Pedro Martins <engpedrocampos@gmail.com>
2019-09-30 14:12:34 -0300

    Enviando imagem via endpoint

--- src/main/java/com/abacoventure/sistemapedidos/services/S3Service.java
+++ src/main/java/com/abacoventure/sistemapedidos/services/S3Service.java
@@ -1,48 +1,61 @@
 package com.abacoventure.sistemapedidos.services;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Service
 public class S3Service {
-	
+
 	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
 
 	@Autowired
 	private AmazonS3 s3client;
-	
+
 	@Value("${s3.bucket}")
 	private String bucketName;
-	
-	public void uploadFile(String localFilePath) {
-		
-		try 
-		{
-			File file = new File(localFilePath);
-			LOG.info("Iniciando upload");
-			s3client.putObject(new PutObjectRequest(bucketName, "teste.png", file));
-			LOG.info("Upload finalizado");
+
+	public URI uploadFile(MultipartFile multipartFile) {
+		try {
+			String fileName = multipartFile.getOriginalFilename();
+			InputStream is = multipartFile.getInputStream();
+			String contentType = multipartFile.getContentType();
+			return uploadFile(is, fileName, contentType);
 		} 
-		catch (AmazonServiceException e ) 
-		{
-			LOG.info("AmazonServiceException: " + e.getErrorMessage());
-			LOG.info("Status code: " + e.getStatusCode());
+		catch (IOException e) {
+			throw new RuntimeException("Erro de IO: " + e.getMessage());
 		}
-		catch(AmazonClientException e) 
-		{
-			LOG.info("AmazonClientException: " + e.getMessage());
-		}
-		
+
 	}
-	
+
+	public URI uploadFile(InputStream inputStream, String fileName, String contentType) {
+		try {
+			ObjectMetadata meta = new ObjectMetadata();
+			meta.setContentType(contentType);
+
+			LOG.info("Iniciando upload");
+			s3client.putObject(bucketName, fileName, inputStream, meta);
+			LOG.info("Upload finalizado");
+
+			return s3client.getUrl(bucketName, fileName).toURI();
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Erro ao converter URL em URI");
+		}
+	}
+
 }
