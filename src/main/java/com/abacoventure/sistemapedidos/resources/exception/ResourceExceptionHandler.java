2995856d9319d811191a734b71316e9ed7bc59e6
Pedro Martins <engpedrocampos@gmail.com>
2019-09-30 09:41:43 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/abacoventure/sistemapedidos/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/abacoventure/sistemapedidos/resources/exception/ResourceExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.abacoventure.sistemapedidos.resources.exception;
 
-import javax.servlet.http.HttpServlet;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
--- src/main/java/com/abacoventure/sistemapedidos/services/PedidoService.java
+++ src/main/java/com/abacoventure/sistemapedidos/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.abacoventure.sistemapedidos.domain.Cliente;
 import com.abacoventure.sistemapedidos.domain.ItemPedido;
 import com.abacoventure.sistemapedidos.domain.PagamentoComBoleto;
 import com.abacoventure.sistemapedidos.domain.Pedido;
@@ -13,6 +17,8 @@ import com.abacoventure.sistemapedidos.domain.enums.EstadoPagamento;
 import com.abacoventure.sistemapedidos.repositories.ItemPedidoRepository;
 import com.abacoventure.sistemapedidos.repositories.PagamentoRepository;
 import com.abacoventure.sistemapedidos.repositories.PedidoRepository;
+import com.abacoventure.sistemapedidos.security.UserSS;
+import com.abacoventure.sistemapedidos.services.exception.AuthorizationException;
 import com.abacoventure.sistemapedidos.services.exception.ObjectNotFoundException;
 
 @Service
@@ -71,6 +77,17 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 	
 }
